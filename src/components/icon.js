2efcfc0c30a889e93ed3a375ce4166ee5407f222
Ramesh Nair <ram@hiddentao.com>
2019-04-25 12:34:36 +0800

    fix lint

--- src/components/icon.js
+++ src/components/icon.js
@@ -2,23 +2,22 @@ import React from 'react'
 
 // From https://github.com/FortAwesome/react-fontawesome/issues/134#issuecomment-471940596
 import "@fortawesome/fontawesome-svg-core/styles.css"
+
 import { config, library } from "@fortawesome/fontawesome-svg-core"
-config.autoAddCss = false;
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 import {
   faTwitter,
   faLinkedin,
   faGithub,
   faStackOverflow
 } from '@fortawesome/free-brands-svg-icons'
-
 import {
   faRss,
   faEnvelope,
   faMapPin,
 } from '@fortawesome/free-solid-svg-icons'
+
+config.autoAddCss = false
 
 library.add(
   faTwitter,
