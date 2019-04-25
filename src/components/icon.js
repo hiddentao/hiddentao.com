e00c739b35ce07ccf204c9f3290945eb4b59e4ba
Ramesh Nair <ram@hiddentao.com>
2019-04-25 12:22:25 +0800

    stop font awesome icon flashing

--- src/components/icon.js
+++ src/components/icon.js
@@ -1,12 +1,19 @@
 import React from 'react'
+
+// From https://github.com/FortAwesome/react-fontawesome/issues/134#issuecomment-471940596
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config, library } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false;
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
+
 import {
   faTwitter,
   faLinkedin,
   faGithub,
   faStackOverflow
 } from '@fortawesome/free-brands-svg-icons'
+
 import {
   faRss,
   faEnvelope,
