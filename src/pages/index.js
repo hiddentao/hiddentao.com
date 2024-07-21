785cfc2798d3c90ad90f884ec3678b8b7a6f28e7
Ramesh Nair <ram@hiddentao.com>
2024-07-21 17:38:38 +0100

    style: remove social links at top

--- src/pages/index.js
+++ src/pages/index.js
@@ -195,16 +195,8 @@ const Page = ({ lang }) => {
               Hey! I'm <em>Ram</em>, a software builder and creative individual.
             </SplashText>
           </SplashTop>
-          <SplashBottom>
-            <TwitterLink />
-            <GithubLink />
-            <LinkedInLink />
-            <EmailLink />
-            <FeedLink />
-          </SplashBottom>
           <SplashDivider />
         </Splash>
-        <h2>Latest writings</h2>
         <StyledPostList posts={posts} />
       </Content>
     </Layout>
